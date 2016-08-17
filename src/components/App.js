80ec9168eddbebac4c6fc8db47186e55ebfa4539
Hoonio <hoonio@hoonio.com>
2016-08-18 00:43:35 +0100

    add header template into react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React, { PropTypes } from 'react';
+import Header from './common/Header';
 
 const App = (props) => {
   return (
     <div className="container-fluid">
+      <Header />
       {props.children}
     </div>
   );
