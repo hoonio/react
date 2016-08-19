9647208db4836cb40c1a84d0666422ae2552ba4d
Hoonio <hoonio@hoonio.com>
2016-08-19 23:46:42 +0100

    add docker image build support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 // import configureStore from './store/configureStore';
